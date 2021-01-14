ec8687657b487e47c3a8ce23392869d690c2fc8b
Ferdousi Haque <ferdousi160228@gmail.com>
2021-01-14 12:59:54 +0600

    Explicit Wait used and xpath changed to cssSelector

--- src/automate/TestCase.java
+++ src/automate/TestCase.java
@@ -1,41 +1,43 @@
 package automate;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class TestCase {
 
 	public static WebDriver driver = null;
-	public static void main(String[] args) throws InterruptedException{
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, 15);
 		
 		// Open the web Browser
 		driver.navigate().to("https://au.godaddy.com/");
 		driver.manage().window().maximize();
 		
 		// Write the text
-		WebElement searchBox = driver.findElement(By.xpath("//input[@name='domainToCheck']"));
+		WebElement searchBox = driver.findElement(By.cssSelector("input[name='domainToCheck']"));
 		searchBox.sendKeys(".net");
 		
 		// Click search button
-		WebElement searchButton = driver.findElement(By.xpath("//button[@aria-label='Search Domain']"));
+		WebElement searchButton = driver.findElement(By.cssSelector("span.searchText"));
 		searchButton.click();
-		Thread.sleep(5000);
+		
 		
 		//Press “Add to Cart” for the first search result
-		WebElement cartButton = driver.findElement(By.xpath("//button[@data-cy='dbs-add-to-cart-button']"));
+		WebElement cartButton;
+		cartButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[data-cy='dbs-add-to-cart-button']")));
 		cartButton.click();
-		Thread.sleep(5000);
 		
 		// Check if the product is added to the cart
-		WebElement addSpan = driver.findElement(By.xpath("//span[@class='text-black']"));
+		WebElement addSpan;
+		addSpan = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='text-black']")));
 		String text = addSpan.getText();
 		
 		if(text.equals("Added")) {
